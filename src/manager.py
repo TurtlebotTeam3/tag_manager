4b17582401de3fe7ea826b164ea865eba4440a41
daniel <daniel@familie-gross.at>
2020-02-25 10:22:50 +0100

    comments

--- src/manager.py
+++ src/manager.py
@@ -21,10 +21,6 @@ class TagManager:
         rospy.on_shutdown(self._shutdown)
         rospy.init_node('tag_manager')
 
-        #script_dir = os.path.dirname(__file__) #<-- absolute dir the script is in
-        #rel_path = "../StoredTag/tags.store"
-        #self.abs_file_path = os.path.join(script_dir, rel_path)
-
         self.abs_file_path=rospy.get_param("~tag_file",default="/home/daniel/catkin_ws/src/tag_manager/StoredTag/tags.store")
 
         self.tag_list = []
@@ -32,10 +28,12 @@ class TagManager:
         self._load_tags()
 
         self.map_info = MapMetaData()
-
         self.markerArray = MarkerArray()
+
+        # --- Publisher ---
         self.marker_publisher = rospy.Publisher('visualization_marker_array', MarkerArray, queue_size=100)
 
+        # --- Services ---
         self.check_tag_known_service = rospy.Service('check_tag_known', CheckTagKnown, self._handle_check_tag_known)
         self.add_tag_service = rospy.Service('add_tag', AddTag, self._handle_add_tag)
         self.get_tags_service = rospy.Service('get_tags', GetTags, self._handle_get_tags)
@@ -45,6 +43,9 @@ class TagManager:
         rospy.spin()
 
     def _setup(self):
+        """
+        Get map meta information
+        """
         map = rospy.wait_for_message('map', OccupancyGrid)
         self.map_info = map.info
 
@@ -53,7 +54,7 @@ class TagManager:
 
     def _handle_check_tag_known(self, data):
         """
-        Checks if the tag at the given x and y coordinates is known
+        Service handler to check if the tag at the given x and y coordinates is known.
         """
         # extract the data
         x = data.x
@@ -65,6 +66,9 @@ class TagManager:
         return CheckTagKnownResponse(response)
 
     def _check_tag_x_y(self, x, y):
+        """
+        Check if the given tag is kown.
+        """
         known = False
         for (y_known, x_known) in self.tag_list:
             if x_known >= (x - self.tag_detection_radius) and x_known <= (x + self.tag_detection_radius) and y_known >= (y - self.tag_detection_radius) and y_known <= (y + self.tag_detection_radius):
@@ -74,7 +78,7 @@ class TagManager:
 
     def _handle_add_tag(self, data):
         """
-        Add a tag to the list of known tags
+        Service handler to add a tag to the list of known tags.
         """
         # extract the data
         x = data.x
@@ -96,7 +100,7 @@ class TagManager:
 
     def _handle_get_tags(self, data):
         """
-        Handle request for tag list
+        Handle request for tag list.
         """
         tag_list = TagList()
 
@@ -110,7 +114,7 @@ class TagManager:
 
     def _load_tags(self):
         """
-        If tags were stored in the fiel previously add it to the list
+        Loads tags from the file if any are stored in the file.
         """
         file = None
         try:
@@ -135,6 +139,9 @@ class TagManager:
                 file.close()
 
     def _store_tags(self):
+        """
+        Store tags to the file.
+        """
         file = None
         try:
             rospy.loginfo("--- Storeing tags ---")
@@ -154,6 +161,9 @@ class TagManager:
             file.close()
 
     def _publish_point(self, x, y):
+        """
+        Publish tag position so they can be visualized in RViz
+        """
         marker = Marker()
         marker.header.frame_id = "Turtle4711/map"
         marker.type = marker.SPHERE
